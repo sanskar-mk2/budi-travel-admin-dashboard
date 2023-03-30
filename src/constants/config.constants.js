8af6f4ecf6764008d5649cc5357b33e54cc1ceee
rajpaliwal <rajpaliwal2011@gmail.com>
2023-03-30 16:02:37 +0530

    url changes

--- src/constants/config.constants.js
+++ src/constants/config.constants.js
@@ -1,4 +1,5 @@
 // export const baseURL = 'http://74.208.150.111/api/admin'; // old 
-export const baseURL = 'http://vinra.api.tiwari.cc/api/admin' // new url 
+// export const baseURL = 'http://vinra.api.tiwari.cc/api/admin' // new url 
+export const baseURL = `https://budi-api.vinratechllp.com/api/admin`
 // export const imgBaseUrl = 'http://74.208.150.111'   // old orl 
 export const imgBaseUrl = 'http://vinra.api.tiwari.cc/' // new url
